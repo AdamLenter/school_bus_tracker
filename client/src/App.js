8f56aac2aa5652ee724af067158802fcdf27d38c
Adam <adamlenter1@gmail.com>
2022-06-01 13:27:59 -0400

    Creeated a home screen. Does not function yet.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import logo from './bus_image.jpeg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const blankLoginFormData = {
+    username: "", 
+    password: ""
+  }
+  const [loginFormData, setLoginFormData] = useState(blankLoginFormData);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>School Bus Tracker</h1>
+      <img src={logo} />
+      <h2>Returning User?</h2>
+      <form>
+        <label>Username: </label>
+        <input value = {setLoginFormData.username} />
+        <br />
+        <br />
+        <label>Password: </label>
+        <input type = "password" value = {setLoginFormData.password} />
+        <br />
+        <br />
+        <button>Submit</button>
+      </form>
+      <p>Click here to create a new account</p>
     </div>
   );
 }
